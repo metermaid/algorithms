7b9c0fc047846d43bb98ab5d80711b74e87f19ee
Rita Cheng <rita@ritacheng.com>
2014-09-20 14:23:14 -0700

    number of ways to make change, a slightly different algo

--- dynamic_programming/making_change.rb
+++ dynamic_programming/making_change.rb
@@ -1,5 +1,5 @@
 # Making Change
-def make_change(amount, coins = [25, 10, 5, 1])
+def make_change(amount, coins = [25, 10, 5, 1]) # find optimal change
   coins.sort!.reverse!
   optimal_change = {}
 
@@ -9,7 +9,7 @@ def make_change(amount, coins = [25, 10, 5, 1])
     elsif coins.include? n
       optimal_change[n] = [n]
     else
-      min_oins = coins.reject {|coin| coin > n}.
+      min_coins = coins.reject {|coin| coin > n}.
       map {|coin| [coin] + optimal_change[n - coin] }.
       reject {|change| change.reduce(0, :+) != n }.
       min {|a,b| a.size <=> b.size}
@@ -18,6 +18,24 @@ def make_change(amount, coins = [25, 10, 5, 1])
   end
   optimal_change[amount]
 end
+
 p make_change(43) # [25, 10, 5, 1, 1, 1]
 p make_change(25) # [25]
 p make_change(0) # nil
+
+def make_change(amount, coins = [25, 10, 5, 1]) # number of ways On^2, On space
+    optimal_change = Array.new(amount+1, 0)
+    optimal_change[0] = 1
+
+    coins.each do |coin|
+      (coin..amount).each do |higher_amount|
+        higher_amount_remainder = higher_amount - coin
+        optimal_change[higher_amount] += optimal_change[higher_amount_remainder]
+      end
+    end
+
+    return optimal_change[amount]
+end
+p make_change(43) # 31
+p make_change(25) # 13
+p make_change(0) # 1
